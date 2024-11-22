82e67dbc449294813932eff953d4fb7675e4d8aa
Tozo sensei <codersensei@outlook.com>
2024-11-21 17:20:50 -0800

    feat(backend): add backend for the project

--- ratetracker/src/App.jsx
+++ ratetracker/src/App.jsx
@@ -5,6 +5,17 @@ import { Link, Outline }  from 'react-router-dom';
 
 
 function App() {
+  const [openNav, setOpenNav] = useState(false);
+
+
+  const handleNavToggle = () => {
+    setOpenNav(true);
+}
+
+const handleNavClose = () => {
+  setOpenNav(false);
+}
+
 
   // const [value, setValue] = useState('')
   // const [rates, setRates] = useState({})
@@ -35,15 +46,33 @@ function App() {
 
 
   return (
-       <div>
-      {/* <form onSubmit={onSearch}>
-        currency: <input value={value} onChange={handleChange} />
-        <button type="submit">exchange rate</button>
-      </form>
-      <pre>
-        {JSON.stringify(rates, null, 2)}
-      </pre> */}
-    </div>
+       <header className='bg-primary-dark flex'>
+        <div>
+          <h1>Logo</h1>
+        </div>
+     
+     <nav className='flex'>
+      <ul>
+        <li>
+          <Link to="/login"  onClick={() => handleNavClose(false)}>Login</Link> 
+        </li>
+        <li>
+          <Link to="/register" onClick={() => handleNavClose(false)}>Register</Link>
+        </li>
+        <li>
+          <Link to="/help" onClick={() => handleNavClose(false)}>Help</Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={() => handleNavClose(false)}>About</Link>
+        </li>
+        <li>
+          <Link to="/contact" onClick={() => handleNavClose(false)}>
+          Contact
+          </Link>
+        </li>
+      </ul>
+     </nav>
+    </header>
   );
 }
 
--- ratetracker/tailwind.config.js
+++ ratetracker/tailwind.config.js
@@ -4,25 +4,37 @@ module.exports = {
     "./src/**/*.{js, jsx,ts,tsx}",
   ],
   theme: {
-    colors : {
-        'primary' : {
-          'dark': '#0C2D48', //moonblue
-          'light' : '#00192E'
-        },
-        'secondary': '#277CA0',
-        'background' : {
-          'light': '#145DA0',
-          'dark': '02427E'
-        },
-        'text' : {
-          'primary': '#FFFFF',
-          'secondary': '#FFEECF'        
-        },
-        'error': '#EA4335',
-        'success': '#34A853', // green
-        'disabled': '#DADCE0' // light grey
+    extend: {
+      colors : {
+        transparent: 'transparent',
+        current: 'currentColor',
+          'primary' : {
+            'dark': '#0C2D48', //moonblue
+            'light' : '#00192E'
+          },
+          'secondary': '#277CA0',
+          'background' : {
+            'light': '#145DA0',
+            'dark': '02427E'
+          },
+          'text' : {
+            'primary': '#FFFFF',
+            'secondary': '#FFEECF'        
+          },
+          'error': '#EA4335',
+          'success': '#34A853', // green
+          'disabled': '#DADCE0' // light grey
+      },
+      screens: {
+        xs: '425px', // Small phones
+        sm: '640px', // Mobile
+        md: '768px', // Tablets
+        lg: '1024px', // Laptop
+        xl: '1280px', // Desktop
+        xxl: '1536px', // Large Screen
+        xxxl: '1920px' // Ultra-large screens
+      }
     },
-    extend: {},
   },
   plugins: [],
 }
