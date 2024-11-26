d519b8658512f348ad50afdfdf591feec21edcc6
Tozo sensei <codersensei@outlook.com>
2024-11-26 14:28:02 -0800

    fix(ui): change the background color for navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,53 +45,51 @@ function App() {
 
   return (
     <div>
-      
+
       <header className="fixed top-0 left-0 right-0 w-full bg-primary-light flex justify-between items-center p-4 text-customText-primary z-10">
 
-      <div className='font-bold'>
-        <h1 className="">CX Logo</h1>
-      </div>
+        <div className='font-bold'>
+          <h1 className="">cX Logo</h1>
+        </div>
 
 
+        <nav className="p-4 ">
+          <button onClick={() => setOpenNav(!openNav)}
+            className='text-2xl focus:outline-none lg:hidden'>
+            <FontAwesomeIcon icon={openNav ? faXmark : faBars} />
+          </button>
 
+          <ul className={`${openNav ? 'block' : 'hidden'} xs:bg-secondary absolute top-full left-0 right-0 w-full text-center lg:static lg:flex lg:bg-transparent lg:gap-x-8 lg:items-start `}>
+            <li className='p-4 lg:p-0 hover:bg-disabled'>
+              <Link to="/help" onClick={() => handleNavClose(false)}>Converter</Link>
+            </li>
 
-      <nav className="p-4 ">
-        <button onClick={() => setOpenNav(!openNav)}
-          className='text-2xl focus:outline-none lg:hidden'>
-          <FontAwesomeIcon icon={openNav ? faXmark : faBars} />
-        </button>
+            <li className='p-4 lg:p-0 hover:bg-disabled hover:text-customText-dark'>
+              <Link to="/contact" onClick={() => handleNavClose(false)}>
+                Tools
+              </Link>
+            </li>
 
-        <ul className={`${openNav ? 'block bg-secondary' : 'hidden'} absolute top-full left-0 right-0 w-full text-center lg:static lg:flex bg-none lg:gap-x-8 lg:items-start `}>
-          <li className='p-4 lg:p-0 hover:bg-disabled'>
-            <Link to="/help" onClick={() => handleNavClose(false)}>Converter</Link>
-          </li>
+            <li className="p-4 lg:p-0 hover:bg-disabled">
+              <Link to="/login" onClick={() => handleNavClose(false)}>Login</Link>
+            </li>
+            <li className='p-4 lg:p-0 hover:bg-disabled'>
+              <button className=' '>
+                <Link to="/register" onClick={() => handleNavClose(false)}>Register</Link>
+              </button>
+            </li>
 
-          <li className='p-4 lg:p-0 hover:bg-disabled hover:text-customText-dark'>
-            <Link to="/contact" onClick={() => handleNavClose(false)}>
-              Tools
-            </Link>
-          </li>
+            <li className='p-4 lg:p-0 hover:bg-disabled'>
+              <Link to="/about" onClick={() => handleNavClose(false)}>About</Link>
+            </li>
 
-          <li className="p-4 lg:p-0 hover:bg-disabled">
-            <Link to="/login" onClick={() => handleNavClose(false)}>Login</Link>
-          </li>
-          <li className='p-4 lg:p-0 hover:bg-disabled'>
-            <button className=' '>
-              <Link to="/register" onClick={() => handleNavClose(false)}>Register</Link>
-            </button>
-          </li>
-
-          <li className='p-4 lg:p-0 hover:bg-disabled'>
-            <Link to="/about" onClick={() => handleNavClose(false)}>About</Link>
-          </li>
-
-        </ul>
-      </nav>
-    </header>
-    <main>
+          </ul>
+        </nav>
+      </header>
+      <main>
         <Outlet />
       </main>
-      </div>
+    </div>
   );
 }
 
