1c3aa38a4349ae2e8391549ba44d4d8032a87486
Tozo sensei <codersensei@outlook.com>
2024-12-11 15:01:20 -0800

    feat(LoginNavbar.jsx): add a navbar component for login and register section

---
+++ frontend/src/components/LoginNavbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const LoginNavbar = () => {
+  return (
+    <header>
+      <nav
+        data-v-achi62s2
+        className="open-ui-navbar"
+        aria-label="Main Navigation"
+      >
+        <div data-v-achi62s2 className="open-ui-navbar-container">
+          <div
+            data-v-achi62s2
+            className="open-ui-navbar-progress-bar"
+            style={{ width: "0%" }}
+          ></div>
+          <div data-v-achi62s2 className="open-ui-navbar-left">
+            <a
+              data-v-0c09xa0
+              href="/"
+              className="open-ui-link"
+              rel="nofollow noreferrer noopener"
+              aria-label="cx Logo"
+            >
+              <span data-v-0c09xa0 className="open-ui-link-text">
+                {/* <img data-v-0291chs src="" alt="cX Logo" srcset="" /> */}
+                <h1>cX Logo</h1>
+              </span>
+            </a>
+          </div>
+          <div data-v-achi62s2 className="open-ui-navbar-center">
+            <div className="information-model"></div>
+            Sign In
+          </div>
+          <div data-v-achi62s2 className="open-ui-navbar-right"></div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default LoginNavbar;
