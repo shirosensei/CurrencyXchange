e68a357f69bdd6077b8d8836ffbff0802df82fd5
Tozo sensei <codersensei@outlook.com>
2024-12-12 14:14:46 -0800

    feat(Footer.jsx): add footer placeholder

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='flex justify-center bg-gradient-to-bl from-900 to blue-700 py-20'>
       <p>&copy; 2024 Xcurrency Inc </p>
     </div>
   )
